4718da6272ee24e8e3404edecbfba490ef8d4303
igrs <specialweekisno1@gmail.com>
2018-03-04 22:05:41 +0900

    Initial commit

---
+++ test/server_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	actual := func(a int, b int) int { return a + b }(10, 20)
+	expected := 30
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestFactorial(t *testing.T) {
+	actual := func(a int) int {
+		for i := a - 1; i > 0; i-- {
+			a *= i
+		}
+		return a
+	}(5)
+	expected := 120
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
